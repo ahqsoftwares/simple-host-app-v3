16414aab90a9a245405fa77833c3c748625a9bd8
AHQMiness <ahqsecret@gmail.com>
2023-01-17 22:49:37 +0530

    add some stuff

--- lib/pages/server/server.dart
+++ lib/pages/server/server.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import "package:flutter/material.dart";
 
 class Server extends StatefulWidget {
@@ -11,8 +13,56 @@ class ServerState extends State<Server> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AppBar(
-        title: const Text("Server Data"),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 0, 52, 89),
+      ),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const TabBar(
+              tabs: [
+                Tab(text: "Console", icon: Icon(Icons.terminal)),
+                Tab(text: "Files", icon: Icon(Icons.folder)),
+                Tab(text: "Settings", icon: Icon(Icons.settings)),
+              ],
+            ),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Container(),
+              ),
+              InkWell(
+                child: Container(
+                  height: 50,
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        child: Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "Back",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
