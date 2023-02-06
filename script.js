e00f32912ebfb300365a01853e7282c0f630f213
AHQ Miness <ahqsecret@gmail.com>
2023-02-06 22:15:31 +0530

    fix everything

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-const buffer = fs.readFileSync(process.env.FILE);
+const buffer = process.env.FILE;
 
 const data = buffer.toString();
 
-fs.writeFileSync("./key2.jks", Buffer.from(data, "base64"));
+fs.writeFileSync("./android/key.jks", Buffer.from(data, "base64"));
